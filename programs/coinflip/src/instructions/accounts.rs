3abce85747f50b47290287a32e88f126334c2443
astrovm <~@4st.li>
2025-01-07 22:08:52 +0000

    Enhance player authorization in JoinGame instruction
    - Added a new constraint to the JoinGame instruction to ensure that the player joining the game is not the creator, improving security by preventing unauthorized actions.
    - This change strengthens the integrity of player interactions and game state management within the coinflip program.

--- programs/coinflip/src/instructions/accounts.rs
+++ programs/coinflip/src/instructions/accounts.rs
@@ -223,6 +223,7 @@ pub struct JoinGame<'info> {
         mut,
         constraint = game.status == GameStatus::Active @ ErrorCode::GameNotActive,
         constraint = !game.players.contains(&player.key()) @ ErrorCode::AlreadyJoined,
+        constraint = game.creator != player.key() @ ErrorCode::UnauthorizedPlayer,
         constraint = game.players.len() < (game.max_players as usize) @ ErrorCode::GameFull,
         constraint = !game.is_private || authority.is_some() && authority.as_ref().unwrap().key() == oracle.authority @ ErrorCode::UnauthorizedPlayer,
     )]
