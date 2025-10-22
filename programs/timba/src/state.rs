1416d6a1dd328bbbe2468b466dcaf3cd46cad587
astro <github.u3vwv@passfwd.com>
2025-10-22 14:41:01 -0300

    Restore InvalidTicketsCount guard when unjoining

--- programs/timba/src/state.rs
+++ programs/timba/src/state.rs
@@ -552,6 +552,9 @@ impl Game {
 
     /// Removes the participant identified by the provided hash and returns the index removed.
     pub fn remove_participant(&mut self, participant_hash: u64) -> Result<usize> {
+        if self.tickets_count == 0 {
+            return err!(ErrorCode::InvalidTicketsCount);
+        }
         let index = self
             .participant_index(participant_hash)
             .ok_or(ErrorCode::UnauthorizedPlayer)?;
